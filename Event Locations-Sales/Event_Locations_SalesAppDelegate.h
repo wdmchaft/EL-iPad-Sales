f3053f545e0cf806c22d18d9a457b10aa56d5a3f
Ujwal Trivedi <Ujwal@ujwal.local>
2011-04-24 15:36:51 -0400

    Initial Commit

---
+++ Event Locations-Sales/Event_Locations_SalesAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  Event_Locations_SalesAppDelegate.h
+//  Event Locations-Sales
+//
+//  Created by Ujwal Trivedi on 4/24/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Event_Locations_SalesAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
